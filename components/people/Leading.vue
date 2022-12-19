f40dfc964fddd9db9c413ef9968b80eedab91b4d
Nick Hanigan <13477700+nick-wireless@users.noreply.github.com>
2022-12-19 23:08:04 +0930

    Feature : About page started with Company info.

---
+++ components/people/Leading.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="bg-white py-16 ">
+    <div class="mx-auto max-w-7xl py-12 px-4 sm:px-6 lg:px-8 lg:py-24">
+      <div class="grid grid-cols-1 gap-12 lg:grid-cols-3 lg:gap-8">
+        <div class="space-y-5 sm:space-y-4 px-4 ">
+          <h2 class="text-3xl font-bold tracking-tight sm:text-2xl">Meet our leadership</h2>
+          <div class="text-lg text-gray-500">
+            <p>Few projects can excite a community, state & country as one that involves an iconic
+              City Stadium, new AFL Team mixed with Indigenous sporting talent.</p>
+            <p class="pt-4 ">This team is committed to making this dream real; tieing the North into Australia and beyond through sport.</p>
+          </div>
+        </div>
+        <div class="pt-6 lg:col-span-2">
+          <ul role="list" class="space-y-12 sm:grid sm:grid-cols-2 sm:gap-12 sm:space-y-0 lg:gap-x-8">
+            <li>
+              <div class="flex items-center space-x-4 lg:space-x-6">
+                <img class="h-16 w-16 rounded-full lg:h-20 lg:w-20" src="../../assets/images/people/nigel.jpg" alt="LinkedIn image of Nigel Brown" />
+                <div class="space-y-1 text-lg font-medium leading-6">
+                  <div class=" flex items-center ">
+                    <h3>Nigel Brown</h3>
+                    <linkedin-and-twitter></linkedin-and-twitter>
+                  </div>
+                  <p class="text-indigo-600">Project Sponsor - Traditional Owner</p>
+                  <p class="text-indigo-600">CEO, Larrakia Development Corp</p>
+                </div>
+
+              </div>
+            </li>
+            <li>
+              <div class="flex items-center space-x-4 lg:space-x-6">
+                <img class="h-16 w-16 rounded-full lg:h-20 lg:w-20" src="../../assets/images/people/sean.jpg"
+                  alt="LinkedIn image of Sean Bowden" />
+                <div class="space-y-1 text-lg font-medium leading-6">
+                  <div class=" flex items-center ">
+                    <h3>Sean Bowden</h3>
+                    <linkedin-and-twitter></linkedin-and-twitter>
+                  </div>
+                  <p class="text-indigo-600">Project Sponsor - AFLNT Chair</p>
+                  <p class="text-indigo-600">McCormack Lawyers & Advisors</p>
+                </div>
+              </div>
+            </li>
+            <li>
+              <div class="flex items-center space-x-4 lg:space-x-6">
+                <img class="h-16 w-16 rounded-full lg:h-20 lg:w-20" src="../../assets/images/people/nick.jpg" alt="LinkedIn image of Nick Hanigan" />
+                <div class="space-y-1 text-lg font-medium leading-6">
+                  <div class=" flex items-center ">
+                    <h3>Nick Hanigan</h3>
+                    <linkedin-and-twitter></linkedin-and-twitter>
+                  </div>
+                  <p class="text-indigo-600">Development Manager</p>
+                  <p class="text-indigo-600">CEO, Stadiums North Australia</p>
+                </div>
+              </div>
+            </li>
+            <li>
+              <div class="flex items-center space-x-4 lg:space-x-6">
+                <img class="h-16 w-16 rounded-full lg:h-20 lg:w-20" src="../../assets/images/people/stuart.jpg"
+                  alt="LinkedIn image of Stuart Harper" />
+                <div class="space-y-1 text-lg font-medium leading-6">
+                  <div class=" flex items-center ">
+                    <h3>Staurt Harper</h3>
+                    <linkedin-and-twitter></linkedin-and-twitter>
+                  </div>
+                  <p class="text-indigo-600">Architect</p>
+                  <p class="text-indigo-600">Director, Stadium Development COX</p>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ pages/Company.vue
@@ -0,0 +1,7 @@
+<template>
+  <nuxt-layout>
+    <people-leading></people-leading>
+    <!-- <people-advising></people-advising> -->
+    <!-- <people-helping></people-helping> -->
+  </nuxt-layout>
+</template>
